693db3810141a4e6246ac31c6e3d73932622da56
gsourabh22 <gsourabh22@gmail.com>
2019-03-24 18:27:43 +0530

    Added Cardview

--- app/src/main/java/com/example/megha/librarymanagement/AdminLoginActivity.java
+++ app/src/main/java/com/example/megha/librarymanagement/AdminLoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.megha.librarymanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminLoginActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(AdminLoginActivity.this,AdminFirstPage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
